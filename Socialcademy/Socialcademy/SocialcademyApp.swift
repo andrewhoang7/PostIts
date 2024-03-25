e49485918738a93dc77695d7140086d6cf16b23e
andrewhoang7 <37169848+andrewhoang7@users.noreply.github.com>
2024-03-25 15:23:26 -0500

    Initial Commit

---
+++ Socialcademy/Socialcademy/SocialcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialcademyApp.swift
+//  Socialcademy
+//
+//  Created by Andrew Hoang on 3/25/24.
+//
+
+import SwiftUI
+
+@main
+struct SocialcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
